cb4f220158cd9c60e42bcd5288009d0c467e1e45
ZanaHashani <117531754+ZanaHashani@users.noreply.github.com>
2022-11-07 10:23:24 +0100

    Update atmproject.py

--- atmproject.py
+++ atmproject.py
@@ -17,22 +17,28 @@ class bank_account():
             return False
 
     def deposit(self):
-        deposit = float(input("Enter amount you want to deposit: "))
-        if deposit >= 0:
-            print(f"You have successfully deposited ${self.deposit}")
-        else:
-            print("You cannot deposit this amount")
-        self.balance += deposit
-        print(f"You now have {self.balance} in your account")
+        try:
+            deposit = float(input("Enter amount you want to deposit: "))
+            if deposit > 0:
+                print(f"You have successfully deposited ${self.deposit}")
+            else:
+                print("You cannot deposit this amount")
+            self.balance += deposit
+            print(f"You now have {self.balance} in your account")
+        except:
+            print("Error. Please enter a value.")
 
     def withdraw(self):
-        withdraw = float(input("Enter amount you want to withdraw: "))
-        if withdraw <= self.balance:
-            print(f"You have successfully withdrawn ${withdraw}")
-        else: 
-            withdraw = float(input("This amount exceeds your balance. Enter a different value: "))
-        self.balance -= withdraw
-        print(f"You now have {self.balance} in your account")
+        try:
+            withdraw = float(input("Enter amount you want to withdraw: "))
+            if withdraw <= self.balance:
+                print(f"You have successfully withdrawn ${withdraw}")
+            else: 
+                withdraw = float(input("This amount exceeds your balance. Enter a different value: "))
+            self.balance -= withdraw
+            print(f"You now have {self.balance} in your account")
+        except:
+            print("Error. Please enter a value.")
 
     def check_balance(self):
         print(f"Your balance is currently: {self.balance}")
@@ -41,21 +47,24 @@ class bank_account():
         return False
 
 a = bank_account("Zana Hashani")
-password = str(input("Enter your PIN: "))
-success = a.login(password)
+while True:
+    password = str(input("Enter your PIN: "))
+    success = a.login(password)
+    if success:
+        break
 while True:
     if(success):
         action = input("Do you want to withdraw (W), deposit (D), check your balance (B), or quit (Q)? ")
-        if action == "W":
+        if action.upper() == "W":
             a.withdraw()
             print(action)
-        elif action == "D":
+        elif action.upper() == "D":
             a.deposit()
             print(action)
-        elif action == "B":
+        elif action.upper() == "B":
             a.check_balance()
             print(action)
-        elif action == "Q":
+        elif action.upper() == "Q":
             a.quit()
             break
 print("------Zana Hashani------")
